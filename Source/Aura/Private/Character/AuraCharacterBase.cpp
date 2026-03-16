88575e15e76e430aa4842798b1d472a91c365b80
Ariana <arianamartin657@gmail.com>
2026-03-16 14:18:38 -0300

    Added Character classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright Arii657
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
